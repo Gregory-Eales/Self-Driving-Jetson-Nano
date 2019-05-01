d37d045c56166f1fcd9e810ba0271d2ee6e392df
Gregory Eales <gregoeales@aol.com>
2019-05-01 11:19:16 -0700

    init controllers

---
+++ interface.py
@@ -0,0 +1,6 @@
+
+
+class Interface_controller(object):
+
+	def __init__(self):
+		pass
--- jetson_controller.py
+++ jetson_controller.py
@@ -2,3 +2,15 @@ import camera
 import motor
 import joystick
 import jetson_ai
+
+
+class JetsonController(object):
+
+	def __init__(self):
+
+		# initialize controlls
+		self.camera = camera.CameraController()
+		self.inferface = interface.InterfaceController()
+		self.motor = motor.MotorController()
+		self.joystick = joystick.JoystickController()
+		
