ec76db8dd80d78f9bab0d69d1d4aa7efc526c4d0
Gregory Eales <gregoeales@aol.com>
2019-05-01 11:03:11 -0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,23 @@
+# dependencies import 
+from cv2 import *
 
+
+# initialize camera controller
 class CameraController(self):
 
 	def __init__(self):
-		pass
+		self.camera = VideoCapture(0)
+
+	def get_image(self):
+
+		# capture image
+		s, image = self.camera.read()
+
+		# if s is true, capture success
+		if s:
+			return image
+
+		# if failure print error message
+		else:
+			print("Error: Unable to capture image")
+			return False
