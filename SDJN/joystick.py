e425a7c9f2e2050a462882f334d16b5adfa233b1
Gregory-Eales <gregoeales@aol.com>
2019-12-03 19:22:25 -0800

    Update joystick.py

--- SDJN/joystick.py
+++ SDJN/joystick.py
@@ -1,37 +1,52 @@
 from inputs import get_gamepad
 import time
+import numpy as np
 
 class JoystickController(object):
 
-	def __init__(object):
+	def __init__(self):
 
-		self.moves = ["BTN_EAST", "BTN_WEST",
-			 "BTN_NORTH", "BTN_SOUTH"]
+		self.directions = ["ABS_RZ", "ABS_Z"]
+		self.right_state = 0
+		self.left_state = 0
+		self.pi = 3.14159
 
-t = time.time()
+	def get_movement_vector(self):
+		return self.forward_state, self.backward_state
+	
+	def normalize(self, n):
+		return (n - 127.5)/127.5
 
-def timer(t, length):
+	def transform(self, forward, backward):
 
-	if (time.time()-t)<length:
-		return True
-	else: return False
+		left = 0
+		right = 0
+		
+		theta = np.arctan(forward/backward)
 
-max_event = 0
-event_types = []
+		theta = theta/self.pi
+		
+		if theta < 0.5: 
+		
+	def get_inputs(self):
+		
+		# event attributes:
+		# event.ev_type, event.code, event.state
 
+		output = [0,0]
+		
+		events = get_gamepad()
+		forward = None
+		backward = None
+		
+		for event in events:
 
-while timer(t, 30):
-	events = get_gamepad()
-	#print(time.time()-t)
-	for event in events:
+			if event.code == self.directions[0]:
+				forward = -self.normalize(event.state)
+			
+			if event.code == self.directions[1]:
+				backward = self.normalize(event.state)
+	
+		if forward == None and backward == None: return None
 
-		if event.ev_type not in event_types:
-			event_types.append(event.ev_type)
-		if event.state > max_event: max_event = event.state
-
-		if event.ev_type == "Key":
-			print(event.ev_type, event.code, event.state)
-			# print("max_state:", max_event)
-
-
-print(event_types)
+		
